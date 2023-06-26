a90b1f2b4b740bff658dcb48362116bb937164bb
triana1983 <137116665+triana1983@users.noreply.github.com>
2023-06-26 16:57:52 -0600

    final changes to hw 17 - OT
    -Final changes made

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,58 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+    public static WebDriver driver = null;
+    public static String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    protected static void enterPassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    protected static void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    protected static void openLoginUrl() {
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+    }
+
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+
+
+    }
+
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,93 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+
+    String newSongAddedNotificationText = "Added 1 song into";
+
+    @Test
+    public void addSongToPlaylist() throws InterruptedException{
+        openLoginUrl();
+        enterEmail("trianasahlin@gmail.com");
+        enterPassword("te$t$tudent");
+        userLogin();
+        openSearchField("ketsa");
+        clickViewAllSearchResult();
+        clickFirstSong();
+        clickAddButton();
+        addToPlayList();
+        Thread.sleep(2000);//we can place it here and selenium will pause for 2 seconds before proceeding with line 22 step
+        Assert.assertTrue(getNotificationText().contains(newSongAddedNotificationText));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public String getNotificationText() throws InterruptedException{
+        WebElement notificationElement = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationElement.getText();
+        //Thread.sleep(2000);
+
+
+    }
+
+    public void addToPlayList() throws InterruptedException{
+//        WebElement choosePlaylist = driver.findElement(By.xpath("//a[@href='#!/playlist/61648']"));
+        WebElement choosePlaylist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Olof Triana Playlist')]"));
+        choosePlaylist.click();
+        Thread.sleep(2000);
+    }
+
+    public void clickAddButton() throws InterruptedException {
+        WebElement clickAdd = driver.findElement(By.cssSelector("button.btn-add-to"));
+        clickAdd.click();
+        Thread.sleep(2000);
+
+    }
+
+    public void clickFirstSong() throws InterruptedException {
+        WebElement firstSong = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song-item td.title"));
+        firstSong.click();
+        Thread.sleep(2000);
+    }
+
+    private void clickViewAllSearchResult() throws InterruptedException{
+        WebElement viewAllSearchResult = driver.findElement(By.cssSelector("div.results section.songs h1 button"));
+        viewAllSearchResult.click();
+        Thread.sleep(2000);
+    }
+
+    private void userLogin() throws InterruptedException{
+        WebElement submitLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitLogin.click();
+        Thread.sleep(2000);
+
+    }
+
+    private void openSearchField(String songName) throws InterruptedException{
+        WebElement searchField = driver.findElement(By.cssSelector("[type='search']"));
+        searchField.click();
+        searchField.clear();
+        searchField.sendKeys(songName);
+        Thread.sleep(2000);
+    }
+
+}
