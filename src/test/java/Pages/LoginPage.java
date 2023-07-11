5e3876996e306364e69be2a75a3c838b36ff0a75
triana1983 <137116665+triana1983@users.noreply.github.com>
2023-07-11 14:16:31 -0600

    homework 23
    final changes made

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
-
-
+    
     public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
     }
