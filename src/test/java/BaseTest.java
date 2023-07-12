11afab761b18e417a3482849398146b3e0a5a536
triana1983 <137116665+triana1983@users.noreply.github.com>
2023-07-12 16:12:41 -0600

    homework 24 changes OT
    installed grid and added code to run grid commands - OT

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -12,6 +17,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -22,10 +29,8 @@ public class BaseTest {
     public static Actions actions = null;
 
 
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
+    //@BeforeSuite
+    //static void setupClass() {WebDriverManager.chromedriver().setup();} REMOVED HW24
 
     public static void enterPassword(String password) {
         WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='password']")));
@@ -45,17 +50,43 @@ public class BaseTest {
         String url = "https://qa.koel.app/";
         driver.get(url);
     }
+    public static WebDriver pickBrowser (String browserName) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.88.202:4444";
+        switch (browserName) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MSEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver(edgeOptions);
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                //added chromeoptions argument to fix websocket error
+                options.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(options);
+                //driver = new ChromeDriver();
+        }
+    }
 
     @BeforeMethod
     @Parameters({"baseUrl"})
-    public void launchBrowser(String baseUrl) {
+    public void launchBrowser(String baseUrl) throws MalformedURLException {
 
-        ChromeOptions options = new ChromeOptions();
-
-        //added chromeoptions argument to fix websocket error
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         // driver = new ChromeDriver(); //ADDED THIS HW20
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
