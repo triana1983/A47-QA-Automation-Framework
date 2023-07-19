79d34fc5138ccaccdcc9e53439a2898f998c051f
triana1983 <137116665+triana1983@users.noreply.github.com>
2023-07-19 17:02:39 -0600

    final changes to homework27
    final

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import Pages.HomePage;
 import Pages.LoginPage;
 
+
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
@@ -17,18 +18,7 @@ public class LoginTests extends BaseTest {
         loginPage.provideEmail("invalid@email.com").providePassword("te$t$tudent").clickSubmit();
 
         Assert.assertEquals(driver.getCurrentUrl(), url); //"https://qa.koel.app/"
-    //
-//      Added ChromeOptions argument below to fix websocket error
-        //ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--remote-allow-origins=*");
 
-        //WebDriver driver = new ChromeDriver(options);
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        //String url = "https://qa.koel.app/";
-        //driver.get(url);
-        //Assert.assertEquals(driver.getCurrentUrl(), url);
-        //driver.quit();
     }
     @Test
     public void loginValidEmailEmptyPasswordTest() {
